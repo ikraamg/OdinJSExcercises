9474534897d4ceff620fa8b3db47b54e3136645b
Ikraam <ikraamghoor@gmail.com>
2020-02-10 19:32:33 +0200

    completed titles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function(books) {
+	return books.map((book) => book.title);
+};
 
 module.exports = getTheTitles;
--- getTheTitles/getTheTitles.spec.js
+++ getTheTitles/getTheTitles.spec.js
@@ -1,19 +1,18 @@
-let getTheTitles = require('./getTheTitles')
+const getTheTitles = require('./getTheTitles');
 
 describe('getTheTitles', function() {
-    const books = [
-      {
-        title: 'Book',
-        author: 'Name'
-      },
-      {
-        title: 'Book2',
-        author: 'Name2'
-      }
-    ]
+	const books = [
+		{
+			title: 'Book',
+			author: 'Name'
+		},
+		{
+			title: 'Book2',
+			author: 'Name2'
+		}
+	];
 
-  it('gets titles', function() {
-    expect(getTheTitles(books)).toEqual(['Book','Book2']);
-  });
-
+	it('gets titles', function() {
+		expect(getTheTitles(books)).toEqual(['Book', 'Book2']);
+	});
 });
