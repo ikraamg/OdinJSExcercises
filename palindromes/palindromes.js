1c54d95e1acf36c0ec1cc7f2a9e3753c0825f6c7
Ikraam <ikraamghoor@gmail.com>
2020-02-10 13:44:27 +0200

    complete palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function() {
+const palindromes = function(text) {
+	const textLowerCase = text.toLowerCase();
+	const textArr = textLowerCase.split('');
 
-}
+	const letterArr = textArr.filter(function(letter) {
+		if (letter <= 'z' && letter.charCodeAt() >= 97) {
+			return true;
+		}
+		return false;
+	});
+	const reverseArr = letterArr.map((x) => x);
+	reverseString = reverseArr.reverse().join('');
+	string = letterArr.join('');
+	console.table(letterArr);
+	console.table(reverseArr);
 
-module.exports = palindromes
+	if (string === reverseString) {
+		return true;
+	}
+	return false;
+};
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+
+module.exports = palindromes;
