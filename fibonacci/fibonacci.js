b8ad8ac055e667e8895868bee0c06a7a989cb7fc
Ikraam <ikraamghoor@gmail.com>
2020-02-10 19:07:20 +0200

    fibonacci complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+	const num = parseInt(value);
+	if (num < 0) return 'OOPS';
+	const fibArray = [];
+	for (let index = 0; index < num; index += 1) {
+		if (index <= 1) {
+			fibArray.push(1);
+		} else fibArray.push(fibArray[index - 2] + fibArray[index - 1]);
+	}
 
-}
+	return fibArray[num - 1];
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
